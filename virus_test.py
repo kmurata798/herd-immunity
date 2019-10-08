b6767671d0c3d88cc71032491d06c101634344ee
kmurata798 <kmurata798@gmail.com>
2019-10-07 17:19:09 -0700

    virus/person tests

---
+++ virus_test.py
@@ -0,0 +1,10 @@
+from virus import Virus
+
+
+def test_virus_instantiation():
+        # TODO: Create your own test that models the virus you are working with
+    '''Check to make sure that the virus instantiator is working.'''
+    virus = Virus("HIV", 0.8, 0.3)
+    assert virus.name == "HIV"
+    assert virus.repro_rate == 0.8
+    assert virus.mortality_rate == 0.3
